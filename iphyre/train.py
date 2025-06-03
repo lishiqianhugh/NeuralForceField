8651cfbf8c538ba1ed9e59830648c240765fea22
Shiqian Li <48059469+lishiqianhugh@users.noreply.github.com>
2025-06-03 17:49:35 +0800

    Add gcn to train.py

--- iphyre/train.py
+++ iphyre/train.py
@@ -65,6 +65,8 @@ elif "slotformer" in model_module.__name__.split(".")[-1]:
                                             slotres_scale=args.slotres_scale, use_dist_mask=args.use_dist_mask)
 elif "in" in model_module.__name__.split(".")[-1]:
     model = model_module.InteractionNetwork(history_len=args.history_len,num_layers=args.layer_num,hidden_dim=args.hidden_dim,use_dist_mask=args.use_dist_mask,angle_scale=args.angle_scale)
+elif "gcn" in model_module.__name__.split(".")[-1]:
+    model = model_module.GCN(history_len=args.history_len,num_layers=args.layer_num,hidden_dim=args.hidden_dim,use_dist_mask=args.use_dist_mask,angle_scale=args.angle_scale)
 else:
     raise ValueError(f"Unknown model name {model_module.__name__}")
 
